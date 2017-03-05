a1a923c40ca87f9a30cdf721b7a57549f641a77c
帶頭二哥 <648959@qq.com>
2017-03-05 19:46:38 +0800

    init

---
+++ part1/1-4.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 最基本的一种，单一条件循环
+	// 这个可以代替其他语言的while循环
+	i := 1
+	for i<3 {
+		fmt.Println(i)
+		i = i + 1
+	}
+
+	// 经典的循环条件初始化/条件判断/循环后条件变化
+	for j := 7; j <= 9; j++ {
+		fmt.Println(j)
+	}
+
+	// 无条件的for循环是死循环，除非你使用break跳出循环或者
+	// 使用return从函数返回
+	for  {
+		fmt.Println("loop")
+		break
+	}
+}
---
+++ part1/1-5.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// 基本的例子
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	// 只有if条件的情况
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	// if条件可以包含一个初始化表达式，这个表达式中的变量
+	// 是这个条件判断结构的局部变量
+	if num := 9; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+}
